b02620faa79f86ee516d9e86401e09679ca4a424
nabhan-au <nabhan.au@gmail.com>
2025-02-07 02:16:10 +0900

    Update table name to follow naming convention

--- database/initData.sql
+++ database/initData.sql
@@ -1,16 +1,16 @@
 USE MealReview
 
-INSERT INTO Restaurant
+INSERT INTO restaurant
 VALUES
     (1, "Ramen Olympus", "https://maps.app.goo.gl/PXQAhE5Dy52RJjvg6"),
     (2, "Soba Olympus", "https://maps.app.goo.gl/XuZNY9tpEgu1pzyLA");
 
-INSERT INTO Category
+INSERT INTO category
 VALUES
     (1, "Ramen"),
     (2, "Soba");
 
-INSERT INTO Review
+INSERT INTO review
 VALUES
     (1, "Olympus", "User1", 5, 1500, "This is very good", "https://lh5.googleusercontent.com/p/AF1QipOVdfjjhcz8n3tO4xCDzkiydiAGhsBR1AJvfsPW=w520-h350-n-k-no", 1, 1),
     (2, "Olympus Extra", "User2", 3, 1300, "This is ok", "https://lh3.googleusercontent.com/p/AF1QipM4xkxd-mfDRgCr2Q8Sp7W8HMNA1YZ0ddG0YdEi=s1360-w1360-h1020", 1, 1),
