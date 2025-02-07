c5ccce7f3a5b242eddad2282ece7f7b62844cbc4
nabhan-au <nabhan.au@gmail.com>
2025-02-07 17:51:53 +0900

    Update review repository

--- server/repositories/restaurantRepository.ts
+++ server/repositories/restaurantRepository.ts
@@ -3,12 +3,10 @@ import { inject, injectable } from "inversify";
 import SERVICE_IDENTIFIER from "../constants/identifiers";
 import { Restaurant } from "../models/restaurant";
 import MySQLConnection from "../utils/mySQLConnection";
-import {Review} from "../models/review";
 
 @injectable()
 class RestaurantRepository {
   private restaurantRepo: Repository<Restaurant>;
-  private reviewRepo: Repository<Review>;
 
   constructor(
     @inject(SERVICE_IDENTIFIER.MYSQL_CONNECTION)
@@ -16,7 +14,6 @@ class RestaurantRepository {
   ) {
     const connection = mysqlConnection.getConnection();
     this.restaurantRepo = connection.getRepository(Restaurant);
-    this.reviewRepo = connection.getRepository(Review);
   }
 
   public async getAllRestaurantsWithReviews(): Promise<Restaurant[]> {
--- server/repositories/reviewRepository.ts
+++ server/repositories/reviewRepository.ts
@@ -1,27 +1,19 @@
 import { Repository } from "typeorm";
 import { inject, injectable } from "inversify";
 import SERVICE_IDENTIFIER from "../constants/identifiers";
-import { Restaurant } from "../models/restaurant";
 import MySQLConnection from "../utils/mySQLConnection";
+import {Review} from "../models/review";
 
 @injectable()
 class ReviewRepository {
-    private restaurantRepo: Repository<Restaurant>;
+    private reviewRepo: Repository<Review>;
 
     constructor(
         @inject(SERVICE_IDENTIFIER.MYSQL_CONNECTION)
         mysqlConnection: MySQLConnection,
     ) {
         const connection = mysqlConnection.getConnection();
-        this.restaurantRepo = connection.getRepository(Restaurant);
-    }
-
-    public async getAllRestaurants() {
-        console.log(this.restaurantRepo.createQueryBuilder("review")
-            .leftJoinAndSelect("review.restaurant", "restaurant") // ✅ Join restaurant
-            .leftJoinAndSelect("review.category", "category") // ✅ Join category
-            .getMany());
-        return await this.restaurantRepo.find();
+        this.reviewRepo = connection.getRepository(Review);
     }
 }
 
