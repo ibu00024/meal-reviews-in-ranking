848cfd86558ca40895fe8bcbb2362affbcd19287
nabhan-au <nabhan.au@gmail.com>
2025-02-14 17:30:24 +0900

    check valid url

--- server/services/imageService.ts
+++ server/services/imageService.ts
@@ -1,16 +1,23 @@
 import { inject, injectable } from "inversify";
 import ImageRepository from "../repositories/imageRepository";
 import SERVICE_IDENTIFIER from "../constants/identifiers";
+import Config from "../config/config";
+import MinioConfig from "../config/minioConfig";
+import minioConfig from "../config/minioConfig";
 
 @injectable()
 class ImageService {
   private imageRepository: ImageRepository;
+  private minioConfig: MinioConfig;
 
   constructor(
     @inject(SERVICE_IDENTIFIER.IMAGE_REPOSITORY)
     imageRepository: ImageRepository,
+    @inject(SERVICE_IDENTIFIER.CONFIG)
+    config: Config
   ) {
     this.imageRepository = imageRepository;
+    this.minioConfig = config.minioConfig;
   }
 
   public async uploadImage(image: Express.Multer.File) {
@@ -27,6 +34,33 @@ class ImageService {
     const id = timestampBits | sequence;
 
     return id.toString();
+  }
+
+  public async isUrlValid(url: string): Promise<boolean> {
+    const extractedPath = this.extractMinioParts(url)
+    const isObjectExist = await this.imageRepository.isObjectExist(extractedPath.object);
+    return extractedPath &&
+        extractedPath.endpoint == this.minioConfig.MINIO_ENDPOINT &&
+        extractedPath.port == this.minioConfig.MINIO_PORT &&
+        extractedPath.bucket == this.minioConfig.MINIO_BUCKET && isObjectExist
+  }
+
+  private extractMinioParts(url: string) {
+    const urlPattern = /http:\/\/([^:]+):(\d+)\/([^\/]+)\/(.+)/;
+    const match = url.match(urlPattern);
+
+    if (!match) {
+      throw new Error("Invalid MinIO URL format");
+    }
+
+    const [, endpoint, port, bucket, objectName] = match;
+
+    return {
+      endpoint: endpoint,
+      port: Number(port),
+      bucket: bucket,
+      object: objectName
+    };
   }
 }
 
