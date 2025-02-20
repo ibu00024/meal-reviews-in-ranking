0408f86456084fcaaccf140de81ae8d1f16b3d27
shirai <haranokuroi@gmail.com>
2025-02-20 12:37:46 +0900

    Adapted to CategoryDTO

--- server/repositories/categoryRepository.ts
+++ server/repositories/categoryRepository.ts
@@ -3,6 +3,7 @@ import { inject, injectable } from "inversify";
 import SERVICE_IDENTIFIER from "../constants/identifiers";
 import MySQLConnection from "../utils/mySQLConnection";
 import { Category } from "../models/category";
+import CategoryDTO from "../models/DTO/categoryDTO";
 
 @injectable()
 class CategoryRepository {
@@ -16,8 +17,9 @@ class CategoryRepository {
         this.categoryRepo = connection.getRepository(Category);
     }
     
-    public async getAllCategories(): Promise<Category[]> {
-        return this.categoryRepo.find();
+    public async getAllCategories(): Promise<CategoryDTO[]> {
+        const categories = await this.categoryRepo.find();
+        return categories.map(category => new CategoryDTO(category.category_id, category.name));
     }
     
 }
