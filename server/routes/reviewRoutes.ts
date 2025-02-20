eb0f161f88ccd837ed9c63ac467c87167559ff3a
nabhan-au <nabhan.au@gmail.com>
2025-02-21 05:43:01 +0900

    add review route

---
+++ server/routes/reviewRoutes.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import container from "../config/ioc_config";
+import SERVICE_IDENTIFIER from "../constants/identifiers";
+import ReviewController from "../controllers/reviewController";
+
+const reviewRouter = express.Router();
+const reviewController = container.get<ReviewController>(
+    SERVICE_IDENTIFIER.REVIEW_CONTROLLER,
+);
+
+reviewRouter.post("/", reviewController.submitReview);
+
+export default reviewRouter;
