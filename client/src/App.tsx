e59767111509272095e03d588d8f4d7bdc5a2e5a
nabhan-au <nabhan.au@gmail.com>
2025-02-21 15:53:11 +0900

    add router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,20 @@
 // import React from "react";
 import HomePage from "./pages/HomePage";
 import { FormDataForm } from "./pages/ReviewPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.tsx";
+import RestaurantPage from "./pages/RestaurantPage.tsx";
 
 const App = () => {
-  //return <HomePage />;
-  return <FormDataForm />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="submit" element={<FormDataForm />} />
+        <Route path="restaurant" element={<RestaurantPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 };
 
 export default App;
